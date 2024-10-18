e5f05c317d9de590970dfa1c538f7c60988c0a8d
mohammedsuhail364 <140154311+mohammedsuhail364@users.noreply.github.com>
2024-10-18 23:13:11 +0530

    Update memorygame.js

--- memorygame.js
+++ memorygame.js
@@ -1,144 +1,140 @@
-const cardsarray=
-[{
-    name:'hippo',
-    // icon:'<i class="fa-solid fa-hippo"></i>'
-    icon:"A"
-
-},
-{
-    name:'bat',
-    // icon:'<i class="fa-duotone fa-bat"></i>'
-    icon:"B"
-},
-{
-    name:'alicorn',
-    // icon:'<i class="fa-sharp fa-solid fa-alicorn"></i>'
-    icon:"C"  
-},
-{
-    name:'deer',
-    // icon:'<i class="fa-sharp fa-regular fa-deer"></i>'
-    icon:"D"
-},
-{
-    name:'ram',
-    // icon:'<i class="fa-sharp fa-regular fa-ram"></i>'
-    icon:"E"
-},
-{
-    name:'monkey',
-    // icon:'<i class="fa-sharp fa-regular fa-monkey"></i>'
-    icon:"F"
-},
-{
-    name:'hippo',
-    // icon:'<i class="fa-solid fa-hippo"></i>'
-    icon:"A"
-
-},
-{
-    name:'bat',
-    // icon:'<i class="fa-duotone fa-bat"></i>'
-    icon:"B"
-},
-{
-    name:'alicorn',
-    // icon:'<i class="fa-sharp fa-solid fa-alicorn"></i>'   
-    icon:"C" 
-},
-{
-    name:'deer',
-    // icon:'<i class="fa-sharp fa-regular fa-deer"></i>'
-    icon:"D"
-},
-{
-    name:'ram',
-    // icon:'<i class="fa-sharp fa-regular fa-ram"></i>'
-    icon:"E"
-},
-{
-    name:'monkey',
-    // icon:'<i class="fa-sharp fa-regular fa-monkey"></i>'
-    icon:"F"
-}
-]
-let flippedcards=[];
-let matchedpairs=0;
-shufflecards();
-const gameboard=document.getElementById('gameboard');
-displaycards();
-function shufflecards()
-{
-    for (let i=cardsarray.length-1;i>=0;i--)
+const cardsarray = [
     {
-        const randindex=Math.floor(Math.random()*(i+1));
-        [cardsarray[i],cardsarray[randindex]]=[cardsarray[randindex],cardsarray[i]]
+        name: 'hippo',
+        icon: "A"
+    },
+    {
+        name: 'bat',
+        icon: "B"
+    },
+    {
+        name: 'alicorn',
+        icon: "C"
+    },
+    {
+        name: 'deer',
+        icon: "D"
+    },
+    {
+        name: 'ram',
+        icon: "E"
+    },
+    {
+        name: 'monkey',
+        icon: "F"
+    },
+    {
+        name: 'hippo',
+        icon: "A"
+    },
+    {
+        name: 'bat',
+        icon: "B"
+    },
+    {
+        name: 'alicorn',
+        icon: "C"
+    },
+    {
+        name: 'deer',
+        icon: "D"
+    },
+    {
+        name: 'ram',
+        icon: "E"
+    },
+    {
+        name: 'monkey',
+        icon: "F"
+    }
+];
+
+let flippedcards = [];
+let matchedpairs = 0;
+
+shufflecards();
+const gameboard = document.getElementById('gameboard');
+displaycards();
+
+function shufflecards() {
+    for (let i = cardsarray.length - 1; i >= 0; i--) {
+        const randindex = Math.floor(Math.random() * (i + 1));
+        [cardsarray[i], cardsarray[randindex]] = [cardsarray[randindex], cardsarray[i]];
     }
 }
-function displaycards()
-{
-    cardsarray.forEach((curr,index,arr)=>
-    {
-        const card=document.createElement('div');
-        card.setAttribute('id',index);
+
+function displaycards() {
+    cardsarray.forEach((curr, index) => {
+        const card = document.createElement('div');
+        card.setAttribute('id', index);
         card.classList.add('cardback');
         card.classList.add('active');
-        gameboard.append(card); 
-        card.addEventListener('click',flipcard);
-    })
+        gameboard.append(card);
+        card.addEventListener('click', flipcard);
+    });
 }
-function flipcard()
-{
-    if (flippedcards.length<2 && this.classList.contains('active'))
-    {
-        let cardid=this.getAttribute('id');
-        // console.log(this);
+
+function flipcard() {
+    // Check if less than 2 cards are flipped and the card is active
+    if (flippedcards.length < 2 && this.classList.contains('active')) {
+        let cardid = this.getAttribute('id');
+
+        // Prevent the same card from being clicked twice
+        if (flippedcards.length === 1 && flippedcards[0].getAttribute('id') === cardid) {
+            return; // Exit if the same card is clicked
+        }
+
+        // Add card to flippedcards and reveal its icon
         flippedcards.push(this);
         this.classList.remove('cardback');
-        // console.log(cardsarray[cardid].icon);
         this.innerHTML = cardsarray[cardid].icon;
     }
-    if(flippedcards.length==2)
-    {
-        setTimeout(checkmatch,1000);
+
+    // Check for match if two cards are flipped
+    if (flippedcards.length == 2) {
+        setTimeout(checkmatch, 1000);
     }
 }
-function checkmatch()
-{
-    const card1id=flippedcards[0].getAttribute('id')
-    const card2id=flippedcards[1].getAttribute('id');
-    if(cardsarray[card1id].name===cardsarray[card2id].name)
-    {
-        flippedcards[0].style.border='none';
-        flippedcards[0].style.backgroundColor='#f5e8ba'
-        flippedcards[0].innerHTML='';
+
+function checkmatch() {
+    const card1id = flippedcards[0].getAttribute('id');
+    const card2id = flippedcards[1].getAttribute('id');
+
+    if (cardsarray[card1id].name === cardsarray[card2id].name) {
+        // Cards match, disable them and change their appearance
+        flippedcards[0].style.border = 'none';
+        flippedcards[0].style.backgroundColor = '#f5e8ba';
+        flippedcards[0].innerHTML = '';
         flippedcards[0].classList.remove('active');
-        flippedcards[1].style.border='none';
-        flippedcards[1].style.backgroundColor='#f5e8ba'
-        flippedcards[1].innerHTML=''
+
+        flippedcards[1].style.border = 'none';
+        flippedcards[1].style.backgroundColor = '#f5e8ba';
+        flippedcards[1].innerHTML = '';
         flippedcards[1].classList.remove('active');
+
         matchedpairs++;
         gameover();
-    }
-    else
-    {
-        flippedcards[0].innerHTML='';
+    } else {
+        // Cards do not match, flip them back
+        flippedcards[0].innerHTML = '';
         flippedcards[0].classList.add('cardback');
-        flippedcards[1].innerHTML='';
+
+        flippedcards[1].innerHTML = '';
         flippedcards[1].classList.add('cardback');
     }
-    flippedcards=[];
 
+    // Clear flipped cards array
+    flippedcards = [];
 }
-function gameover()
-{
-    if (matchedpairs == cardsarray.length/2)
-    {
-        while (gameboard.firstChild)
-        {
+
+function gameover() {
+    if (matchedpairs == cardsarray.length / 2) {
+        // Remove all cards from the game board
+        while (gameboard.firstChild) {
             gameboard.removeChild(gameboard.firstChild);
         }
-        gameboard.innerHTML="YOU WON";
+        // Display win message
+        gameboard.innerHTML = "YOU WON";
         gameboard.classList.remove('game');
         gameboard.classList.add('won');
     }
